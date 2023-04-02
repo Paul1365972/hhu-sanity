b0cf1f83284bd291d4ac2032033a3fa41df48209
paul1365972 <paul1365972@gmail.com>
2023-04-03 01:02:58 +0200

    Fix minor bugs and remove logging

--- hhu-sanity-pretty-lsf/markonline.js
+++ hhu-sanity-pretty-lsf/markonline.js
@@ -1,7 +1,7 @@
 (function () {
   const url = new URL(window.location.href);
   const searchParams = url.searchParams;
-  if (searchParams.get("P.vx") !== "kurz")
+  if (searchParams.has("P.vx") && searchParams.get("P.vx") !== "kurz")
    return;
   searchParams.set('P.vx', 'mittel');
   url.search = searchParams.toString();
@@ -11,9 +11,6 @@
   .then(html => {
     const parser = new DOMParser();
     const doc = parser.parseFromString(html, 'text/html');
-    console.log("Parsed");
-
-
     const notes = doc.querySelectorAll("tr > td.notiz");
 
     let modified = 0;
@@ -25,10 +22,14 @@
         // Maybe replace this by relative instead of matching by title, but this approach does not desync which is nice
         const parallels = document.querySelectorAll(`tbody:nth-child(1) > tr > td.klein > a.ver[title='${name}'`);
         parallels.forEach(p => {
-          p.classList.add("hhu-sanity-online");
+          if (!p.classList.contains("hhu-sanity-online")) {
+            p.classList.add("hhu-sanity-online");
+            modified += 1;
+          }
         });
       }
-    })
+    });
+
     console.log(`[LSF Online Marker] Marked ${modified} elements as online`);
   })
   .catch(error => console.error(error));
